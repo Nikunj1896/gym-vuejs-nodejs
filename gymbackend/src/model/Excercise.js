2769b88ead49df3fbfd73bdf1cbed27ac4983a36
Nikunj1896 <nikunj.projects9893@gmail.com>
2023-05-09 09:30:21 +0530

    change model of excersice

--- gymbackend/src/model/Excercise.js
+++ gymbackend/src/model/Excercise.js
@@ -1,53 +1,59 @@
 const mongoose = require("mongoose");
 
 const excerciseSchema = new mongoose.Schema({
-    Category: {
-        type: String,
-        required: true
-      },
-      Difficulty: {
-        type: String,
-        required: true
-      },
-      Force: {
-        type: String,
-        required: false
-      },
-      Grips: {
-        type: String,
-        required: false
-      },
-      details: {
-        type: String,
-        required: false
-      },
-      exercise_name: {
-        type: String,
-        required: true
-      },
-      steps: {
-        type: [String],
-        required: true
-      },
-      target: {
-        Primary: {
-          type: [String],
-          required: true
-        },
-        Secondary: {
-          type: [String],
-          required: true
-        }
-      },
-      videoURL: {
-        type: [String],
-        required: true
-      },
-      youtubeURL: {
-        type: String,
-        required: false
-      }
+  Category: {
+    type: String,
+    required: true
+  },
+  id:{
+    type:Number,
+    required:true,
+  },
+  Difficulty: {
+    type: String,
+    required: true
+  },
+  Force: {
+    type: String,
+    required: false
+  },
+  Grips: {
+    type: String,
+    required: false
+  },
+  details: {
+    type: String,
+    required: false
+  },
+  exercise_name: {
+    type: String,
+    required: true
+  },
+  steps: {
+    type: [String],
+    required: true
+  },
+  target: {
+    Primary: {
+      type: [String],
+      required: true
+    },
+    Secondary: {
+      type: [String],
+      required: true
+    }
+  },
+  videoURL: {
+    type: [String],
+    required: true
+  },
+  youtubeURL: {
+    type: String,
+    required: false
+  }
 });
 
-let Exersise = mongoose.model("Excercise", excerciseSchema);
+
+let Exersise = mongoose.model("Excercise1", excerciseSchema);
 module.exports = Exersise;
+
